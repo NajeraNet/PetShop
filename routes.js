efd6dd3a5283cebea68763ce9a3f1e7ac440838f
Catherine Hong <catherinehhong@gmail.com>
2016-03-22 17:07:02 -0700

    added regex match

--- routes.js
+++ routes.js
@@ -2,7 +2,6 @@ var fs = require('fs');
 var path = require('path');
 var pets;
 var petNum = process.argv[3];
-var url = require('url');
 var petsPath = path.join(__dirname, 'pets.json');
 fs.readFile(petsPath, 'utf8', function(err, data) {
   if (err) {
@@ -10,7 +9,6 @@ fs.readFile(petsPath, 'utf8', function(err, data) {
   }
   pets = JSON.parse(data);
 });
-console.log(petNum);
 
 var routes = {
   '/pets': function(req, res) {
@@ -18,16 +16,28 @@ var routes = {
    res.statusCode = 200;
    res.end(JSON.stringify(pets));
  },
- '/pets/0': function(req, res) {
-   res.setHeader("Content-Type", "application/json");
-   res.statusCode = 200;
-   res.end(JSON.stringify(pets[0]));
- },
- '/pets/1': function(req, res) {
-   res.setHeader("Content-Type", "application/json");
-   res.statusCode = 200;
-   res.end(JSON.stringify(pets[1]));
- }
+  pethandler: function(req, res, index) {
+    if (index <= JSON.stringify(pets).length -1 && index >= 0) {
+    res.setHeader("Content-Type", 'application/json');
+    res.statusCode = 200;
+    res.end(JSON.stringify(pets[index]));
+  } else {
+    res.statusCode = 404;
+    res.setHeader("Content-Type", "text/plain");
+    res.end("Not Found")
+  }
+  }
+ //,
+//  '/pets/0': function(req, res) {
+//    res.setHeader("Content-Type", "application/json");
+//    res.statusCode = 200;
+//    res.end(JSON.stringify(pets[0]));
+//  },
+//  '/pets/1': function(req, res) {
+//    res.setHeader("Content-Type", "application/json");
+//    res.statusCode = 200;
+//    res.end(JSON.stringify(pets[1]));
+//  }
 }
 
 module.exports = routes;

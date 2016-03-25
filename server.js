f7ac5a0adb03b266008769162fd4b86f8621b2aa
Catherine Hong <catherinehhong@gmail.com>
2016-03-24 22:43:26 -0700

    post, put, and delete work

--- server.js
+++ server.js
@@ -1,72 +1,122 @@
-var express = require('express')();
+'use strict';
+
+var express = require('express');
 var app = express();
 var fs = require('fs');
 var path = require('path');
 var petsPath = path.join(__dirname, 'pets.json');
 var pets;
+
+app.disable('x-powered-by');
+
+
+var morgan = require('morgan');
+app.use(morgan('short'));
+
 var bodyParser = require('body-parser');
-var multer = require('multer');
-var upload = multer();
+app.use(bodyParser.json());
 
 app.set('view engine', 'ejs');
-app.use(bodyParser.json());
+app.set('port', process.env.PORT || 5000);
 
 fs.readFile(petsPath, 'utf8', function(err, data) {
   if (err) {
     throw err;
-    res.status(200);
   }
   pets = JSON.parse(data);
-  app.listen(3000, function() {
-    console.log('listening');
+
+  app.listen(app.get('port'), function() {
+    console.log('listening on', app.get('port'))
   })
 });
-
 
 app.get('/pets', function(req, res) {
-    res.render('index', {
-      pets: JSON.stringify(pets),
+  res.render('index', {
+    pets: pets,
   });
 });
 
-app.get('/pets/0', function(req, res) {
-  res.render('index', {
-    pets: JSON.stringify(pets[0]),
-  });
-});
+app.get('/pets/:index', function(req, res) {
+  var index = Number.parseInt(req.params.index);
 
-app.get('/pets/1', function(req, res) {
-  res.render('index', {
-    pets: JSON.stringify(pets[1])
-  })
-})
-
-app.post('/pets/', upload.array(), function(req, res, next) {
-
-  var petobj = {
-    age: age,
-    kind: kind,
-    name: name
+  if (Number.isNaN(index) || index < 0 || index >= pets.length) {
+    return res.sendStatus(404);
   }
-  body.push(petobj);
-  res.json(req.body);
 
   res.render('index', {
-    pets: JSON.stringify(body)
-  })
-  // var petsJSON = JSON.stringify(pets);
-  // fs.writeFile(petsPath, petsJSON, function(updateErr) {
-  //   if (updateErr) {
-  //     throw updateErr;
-      // res.statusCode(400).send('Bad Request')
-    // }
-  // })
-})
+    pets: JSON.stringify(pets[index]),
+  });
+});
 
 app.get('/*', function(req, res) {
   res.set("Content-Type", "text/plain");
   res.status(404).send("Not found");
 })
+
+app.post('/pets', function(req, res) {
+  var pet = req.body;
+
+  if (Number.isNaN(pet.age) || pet.kind !== "" || pet.name !== "") {
+    return res.sendStatus(400);
+  }
+
+  pets.push(pet);
+
+  var petsJSON = JSON.stringify(pets);
+  fs.writeFile(petsPath, petsJSON, function(updateErr) {
+    if (updateErr) {
+      throw updateErr;
+      res.statusCode(400).send('Bad Request')
+    } else {
+      res.render('index', {
+        pets: pet,
+      })
+    }
+  })
+})
+
+app.put('/pets/:index', function(req, res) {
+  var index = Number.parseInt(req.params.index);
+
+  if (Number.isNaN(index) || index < 0 || index >= pets.length) {
+    return res.sendStatus(404);
+  }
+
+  var pet = req.body;
+  pets[index] = pet;
+
+  if (Number.isNaN(pet.age) || pet.kind === "" || pet.name === "") {
+    return res.sendStatus(400);
+  }
+
+  var petsJSON = JSON.stringify(pets);
+
+  fs.writeFile(petsPath, petsJSON, function(updateErr) {
+    if (updateErr) {
+      throw updateErr;
+      res.statusCode(400).send('Bad Request')
+    } else {
+      res.render('index', {
+        pets: pet,
+      })
+    }
+  })
+})
+
+app.delete('/pets/:index', function(req, res) {
+  var index = Number.parseInt(req.params.index);
+
+  if (Number.isNaN(index) || index < 0 || index >= pets.length) {
+    return res.sendStatus(400);
+  }
+
+  var pet = pets.splice(index, 1) [0];
+
+  res.render('index', {
+    pets: pets
+  })
+});
+
 
 
 // set.header('Content-Type', "text/")

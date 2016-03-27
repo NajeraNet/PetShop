4898a7ea0a37c93e8265c8089059db4b5ed1d41c
Catherine Hong <catherinehhong@gmail.com>
2016-03-26 21:14:39 -0700

    patch method works, added error handling middleware

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 'use strict';
 
-var express = require('express');
-var app = express();
-var fs = require('fs');
-var path = require('path');
-var petsPath = path.join(__dirname, 'pets.json');
-var pets;
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const path = require('path');
+const petsPath = path.join(__dirname, 'pets.json');
+let pets;
 
 app.disable('x-powered-by');
 
 
-var morgan = require('morgan');
+const morgan = require('morgan');
 app.use(morgan('short'));
 
-var bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
 app.set('view engine', 'ejs');
@@ -26,8 +26,8 @@ fs.readFile(petsPath, 'utf8', function(err, data) {
   pets = JSON.parse(data);
 
   app.listen(app.get('port'), function() {
-    console.log('listening on', app.get('port'))
-  })
+    console.log('listening on', app.get('port'));
+  });
 });
 
 app.get('/pets', function(req, res) {
@@ -36,11 +36,14 @@ app.get('/pets', function(req, res) {
   });
 });
 
-app.get('/pets/:index', function(req, res) {
-  var index = Number.parseInt(req.params.index);
+app.get('/pets/:index', function(req, res, next) {
+  const index = Number.parseInt(req.params.index, 10);
 
   if (Number.isNaN(index) || index < 0 || index >= pets.length) {
-    return res.sendStatus(404);
+    Item.find(function(err, items) {
+      if (err) return next(err);
+      console.log(items);
+    })
   }
 
   res.render('index', {
@@ -49,12 +52,12 @@ app.get('/pets/:index', function(req, res) {
 });
 
 app.get('/*', function(req, res) {
-  res.set("Content-Type", "text/plain");
-  res.status(404).send("Not found");
-})
+  res.set('Content-Type', 'text/plain');
+  res.status(404).send('Not found');
+});
 
 app.post('/pets', function(req, res) {
-  var pet = req.body;
+  const pet = req.body;
 
   if (Number.isNaN(pet.age) || !pet.kind || !pet.name) {
     return res.sendStatus(400);
@@ -62,89 +65,93 @@ app.post('/pets', function(req, res) {
 
   pets.push(pet);
 
-  var petsJSON = JSON.stringify(pets);
+  const petsJSON = JSON.stringify(pets);
   fs.writeFile(petsPath, petsJSON, function(updateErr) {
     if (updateErr) {
       throw updateErr;
-      res.statusCode(400).send('Bad Request')
+      res.statusCode(400).send('Bad Request');
     } else {
       res.render('index', {
         pets: pet,
-      })
+      });
     }
-  })
-})
+  });
+});
 
 app.put('/pets/:index', function(req, res) {
-  var index = Number.parseInt(req.params.index);
+  const index = Number.parseInt(req.params.index, 10);
 
   if (Number.isNaN(index) || index < 0 || index >= pets.length) {
     return res.sendStatus(404);
   }
 
-  var pet = req.body;
+  const pet = req.body;
   pets[index] = pet;
 
   if (Number.isNaN(pet.age) || !pet.kind || !pet.name) {
     return res.sendStatus(400);
   }
 
-  var petsJSON = JSON.stringify(pets);
+  const petsJSON = JSON.stringify(pets);
 
   fs.writeFile(petsPath, petsJSON, function(updateErr) {
     if (updateErr) {
       throw updateErr;
-      res.statusCode(400).send('Bad Request')
+      res.statusCode(400).send('Bad Request');
     } else {
       res.render('index', {
         pets: pet,
-      })
+      });
     }
-  })
-})
+  });
+});
+
+app.patch('/pets/:index', function(req, res) {
+  const index = Number.parseInt(req.params.index, 10);
+
+  if (Number.isNaN(index) || index < 0 || index >= pets.length) {
+    return res.sendStatus(404);
+  }
+
+  let pet = pets[index];
+  for (let prop in req.body) {
+    pet[prop] = req.body[prop]
+  }
+
+  pets[index] = pet;
+
+  if (Number.isNaN(pet.age) || pet.kind === '' || pet.name === '') {
+    return res.sendStatus(400);
+  }
+
+  const petsJSON = JSON.stringify(pets);
+
+  fs.writeFile(petsPath, petsJSON, function(updateErr) {
+    if (updateErr) {
+      throw updateErr;
+      res.statusCode(400).send('Bad Request');
+    } else {
+      res.render('index', {
+        pets: pet,
+      });
+    }
+  });
+});
 
 app.delete('/pets/:index', function(req, res) {
-  var index = Number.parseInt(req.params.index);
+  const index = Number.parseInt(req.params.index, 10);
 
   if (Number.isNaN(index) || index < 0 || index >= pets.length) {
     return res.sendStatus(400);
   }
 
-  var pet = pets.splice(index, 1) [0];
+  const pet = pets.splice(index, 1)[0];
 
   res.render('index', {
-    pets: pet
-  })
+    pets: pet,
+  });
 });
 
-
-
-// set.header('Content-Type', "text/")
-// var handleRequest = function(req, res) {
-//   var petRegExp = /^\/pets\/(.*)$/;
-//   var matchy = req.url.match(petRegExp);
-//
-//   if (req.method === 'GET') {
-//     if (routes[req.url]) {
-//       routes[req.url](req, res);
-//     } else if (matchy) {
-//       routes.pethandler(req, res, matchy[1]);
-//     }
-//   }
-//   if (req.method === 'POST') {
-//     var body = [];
-//     if (req.url === "/pets") {
-//       req.on('data', function(chunk) {
-//         body.push(chunk);
-//       }).on('end', function() {
-//         body = Buffer.concat(body).toString();
-//         body = JSON.parse(body);
-//         routes.petPostHandler(req, res, body);
-//       });
-//     }
-//   } else {
-//     res.statusCode = 404;
-//     res.setHeader("Content-Type", "text/plain");
-//     res.end("Not Found")
-//   }
-// }
+app.use(function(err, req, res, next) {
+  return res.send(500, { message: err.message });
+});
